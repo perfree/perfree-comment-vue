b9ae6acc2bba9c76c347c7862b0460fdf84f65d4
perfree <perfree@126.com>
2022-08-19 15:01:21 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <comment></comment>
+  </div>
+</template>
+
+<script>
+import Comment from './components/Comment.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Comment
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Comment.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="perfree-comment">
+        评论组件
+    </div>
+</template>
+<script>
+export default {
+    name: 'comment'
+}
+</script>
